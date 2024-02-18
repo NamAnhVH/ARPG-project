cec6000476863dd5e42f3f04ff8c63bdf5cfdb79
NamAnhVH <namanh12022002@gmail.com>
2024-02-18 12:35:27 +0700

    Thêm trang bị vũ khí + chuyển trạng thái sang sử dụng vũ khí + hoạt ảnh di chuyển có vũ khí (sword)

---
+++ sources/Scripts/hitbox.gd
@@ -0,0 +1,50 @@
+extends Area2D
+
+signal damaged(amount, knockback_strength, damage_source, attacker)
+signal immunity_ended()
+signal feature_damaged()
+
+#const HIT_EFFECT = preload("res://Effects/HitEffect.tscn")
+
+@onready var immunity_timmer = $ImmunityTimer
+@onready var parent = get_parent()
+
+@export var immunity_duration := 0.0
+
+var exceptions = []
+
+func add_exception(node: Node2D): # Them node vao danh sach exception va tu xoa khoi danh sach neu doi tuong bi pha huy
+	if node != null:
+		exceptions.append(node)
+		#node.connect("tree_exiting", self, "remove_exception", [node])
+
+func remove_exception(node: Node2D):
+	if node in exceptions:
+		exceptions.erase(node)
+
+func _on_area_entered(area):
+	if area in exceptions || !immunity_timmer.is_stopped():
+		return
+	
+	if area is DamageArea:
+		if !(self in area.exceptions):
+			if parent is BattleCharacter:
+				damage(area.damage_amount, area.knockback_strength, area, area.attacker)
+				area.on_hit(self)
+				#var effect = HIT_EFFECT.instance()
+				#var main = get_tree().current_scene
+				#main.add_child(effect)
+				#effect.global_position = global_position
+			else:
+				emit_signal("feature_damaged")
+
+func damage(amount, knockback_strength, damage_source: Area2D, attacker):
+	emit_signal("damaged", amount, knockback_strength, damage_source, attacker)
+	if immunity_duration > 0:
+		immunity_timmer.start(immunity_duration) #Bat dau thoi gian bat tu
+
+func _on_immunity_timer_timeout():
+	emit_signal("immunity_ended")
+
+
+
--- sources/Scripts/playable_character.gd
+++ sources/Scripts/playable_character.gd
@@ -1,12 +1,33 @@
-extends CharacterBody2D
+extends BattleCharacter
 class_name PlayableCharacter
 
 @export var move_speed_unit = 5 #toc do di chuyen cua nhan vat
 
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
+@onready var body : Node2D = $Body
+@onready var base : Sprite2D = $Body/Base
+@onready var one_hand_weapon : Sprite2D = $Body/OneHandWeapon
 
 var move_input := Vector2()
+
+func _ready():
+	base.texture = load("res://assets/characters/base/standMovePush/humn_v00.png")
+	#one_hand_weapon.texture = load("res://assets/Weapon/Axe/char_a_pONE3_6tla_ax01_v01.png")
+	
+	#Chỉnh sửa sau (Đang dùng để chỉnh trạng thái trang bị vũ khí)
+	animation_tree.set("parameters/conditions/is_unequip_weapon", true)
+
+func _unhandled_input(event):
+	#Chỉnh sửa sau (Đang dùng để chỉnh trạng thái trang bị vũ khí)
+	if event.is_action_pressed("change_weapon"):
+		if animation_tree.get("parameters/conditions/is_unequip_weapon"):
+			equip_sword_shield()
+			animation_tree.set("parameters/conditions/is_unequip_weapon", false)
+		else:
+			unequip_weapon()
+			animation_tree.set("parameters/conditions/is_unequip_weapon", true)
+
 
 func _physics_process(_delta):
 	move_state()
@@ -23,10 +44,26 @@ func move_state():
 	if move_input != Vector2.ZERO:
 		animation_tree.set("parameters/Stand/blend_position", move_input)
 		animation_tree.set("parameters/Run/blend_position", move_input)
-		animation_state.travel("Run")
+		animation_tree.set("parameters/Run_sword_shield/blend_position", move_input)
+		animation_tree.set("parameters/Stand_sword_shield/blend_position", move_input)
+		animation_tree.set("parameters/conditions/is_running", true)
+		animation_tree.set("parameters/conditions/is_stand", false)
+		#animation_state.travel("Run")
 	else:
-		animation_state.travel("Stand")
+		animation_tree.set("parameters/conditions/is_running", false)
+		animation_tree.set("parameters/conditions/is_stand", true)
+		#animation_state.travel("Stand")
 	
 	#Vecto di chuyen
 	velocity = lerp(velocity, move_input * move_speed_unit * 24, 1)
 	move_and_slide()
+
+#Chỉnh sửa sau
+func equip_sword_shield():
+	one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/standMovePush/axe_v00.png")
+	animation_tree.set("parameters/conditions/is_equip_sword_shield", true)
+
+#Chỉnh sửa sau
+func unequip_weapon():
+	one_hand_weapon.texture = null
+	animation_tree.set("parameters/conditions/is_equip_sword_shield", false)
