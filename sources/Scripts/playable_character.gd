7d5bcff988182b4ff18d1714ba59308a555d44f8
NamAnhVH <namanh12022002@gmail.com>
2024-02-25 20:28:09 +0700

    Thêm hoạt ảnh rút vũ khí + cất vũ khí, Thêm trạng thái sẵn sàng tấn công, Thêm hoạt ảnh di chuyển khi ở trạng thái sẵn sàng tấn công

--- sources/Scripts/playable_character.gd
+++ sources/Scripts/playable_character.gd
@@ -1,6 +1,9 @@
 extends BattleCharacter
 class_name PlayableCharacter
 
+const BIAS = Vector2(0, 5)
+
+
 @export var move_speed_unit = 5 #toc do di chuyen cua nhan vat
 
 @onready var animation_tree = $AnimationTree
@@ -11,17 +14,19 @@ class_name PlayableCharacter
 
 var is_unequip_weapon : bool = true #Check trang bị vũ khí chưa (Chỉnh sửa sau)
 var is_attacking : bool = false
+var is_attack_state: bool = false
+var is_drawing : bool = false
+var is_sheathing : bool = false
 var attack_combo : int = 0
 
 var move_input := Vector2()
+var mouse := Vector2()
 
 func _ready():
 	#Gán texture khi được khởi tạo
 	base.texture = load("res://assets/characters/base/standMovePush/humn_v00.png")
-	
-	#Chỉnh sửa sau (Đang dùng để chỉnh trạng thái trang bị vũ khí)
-	animation_tree.set("parameters/conditions/is_unequip_weapon", true)
 
+#Xử lý thao tác
 func _unhandled_input(event):
 	#Chỉnh sửa sau (Đang dùng để chỉnh trạng thái trang bị vũ khí)
 	if event.is_action_pressed("change_weapon"):
@@ -29,11 +34,22 @@ func _unhandled_input(event):
 			equip_weapon()
 		else:
 			unequip_weapon()
+	
+	if event.is_action_pressed("draw_sheath"):
+		if !is_unequip_weapon:
+			if is_attack_state:
+				sheath()
+			else:
+				draw()
+	
+	#Nhấn phím Attack
+	if event.is_action_pressed("attack"):
+		if !is_attacking and !is_unequip_weapon and !is_drawing and !is_sheathing and is_attack_state:
+			attack()
 
 func _physics_process(_delta):
-	if !is_attacking:
+	if !is_attacking and !is_drawing and !is_sheathing:
 		move_state()
-	attack_state()
 
 #Trạng thái di chuyển của nhân vật
 func move_state():
@@ -45,20 +61,24 @@ func move_state():
 	
 	#Animation
 	if move_input != Vector2.ZERO:
-		animation_tree.set("parameters/Stand/blend_position", move_input)
-		animation_tree.set("parameters/Run/blend_position", move_input)
-		animation_tree.set("parameters/Sword_shield_attack/Slash_1/blend_position", move_input)
-		animation_tree.set("parameters/Sword_shield_attack/Slash_2/blend_position", move_input)
-		animation_tree.set("parameters/Sword_shield_attack/Thrust/blend_position", move_input)
-		animation_tree.set("parameters/Sword_shield_attack/Shield_bash/blend_position", move_input)
-		animation_tree.set("parameters/conditions/is_running", true)
-		animation_tree.set("parameters/conditions/is_standing", false)
-
+		animation_tree.set("parameters/Base/Stand/blend_position", move_input)
+		animation_tree.set("parameters/Base/Run/blend_position", move_input)
+		animation_tree.set("parameters/Base/conditions/is_running", true)
+		animation_tree.set("parameters/Base/conditions/is_standing", false)
+		
+		animation_tree.set("parameters/Sword_shield/Draw/blend_position", move_input)
+		animation_tree.set("parameters/Sword_shield/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Sword_shield/Move/blend_position", move_input)
+		animation_tree.set("parameters/Sword_shield/Sheath/blend_position", move_input)
+		animation_tree.set("parameters/Sword_shield/conditions/is_moving", true)
+		animation_tree.set("parameters/Sword_shield/conditions/is_idling", false)
 	else:
-		animation_tree.set("parameters/conditions/is_running", false)
-		animation_tree.set("parameters/conditions/is_standing", true)
+		animation_tree.set("parameters/Base/conditions/is_running", false)
+		animation_tree.set("parameters/Base/conditions/is_standing", true)
+		
+		animation_tree.set("parameters/Sword_shield/conditions/is_moving", false)
+		animation_tree.set("parameters/Sword_shield/conditions/is_idling", true)
 
-	
 	#Vecto di chuyen
 	velocity = lerp(velocity, move_input * move_speed_unit * 24, 1)
 	move_and_slide()
@@ -75,31 +95,97 @@ func unequip_weapon():
 	shield.texture = null
 	is_unequip_weapon = true
 
-func attack_state():
-	if !is_attacking && !is_unequip_weapon:
-		if Input.is_action_just_pressed("attack"):
-			base.texture = load("res://assets/characters/swordAndShieldBase/attack/humn_v00.png")
-			one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/attack/axe_v00.png")
-			shield.texture = load("res://assets/weapon/swordAndShield/attack/shield_v00.png")
-			is_attacking = true
-			animation_tree.set("parameters/conditions/is_attacking", is_attacking)
-			attack_timer.start()
-			animation_tree.set("parameters/Sword_shield_attack/conditions/is_slash_1", attack_combo == 0)
-			animation_tree.set("parameters/Sword_shield_attack/conditions/is_slash_2", attack_combo == 1)
-			animation_tree.set("parameters/Sword_shield_attack/conditions/is_thrust", attack_combo == 2)
-			animation_tree.set("parameters/Sword_shield_attack/conditions/is_shield_bash", attack_combo == 3)
-			if attack_combo < 3:
-				attack_combo += 1
-			else:
-				attack_combo = 0
+func attack():
+	is_attacking = true
+	attack_timer.start()
+	mouse = (get_global_mouse_position() - global_position - BIAS).normalized()
+	set_attack_animation(mouse, attack_combo)
+	if attack_combo < 3:
+		attack_combo += 1
+	else:
+		attack_combo = 0
 
+func draw():
+	is_drawing = true
+	animation_tree.set("parameters/conditions/is_drawing", is_drawing)
+
+func sheath():
+	is_attack_state = false
+	is_sheathing = true
+	animation_tree.set("parameters/Sword_shield/conditions/is_sheathing", is_sheathing)
+
+#Thiết lập hoạt ảnh đòn đánh
+func set_attack_animation(_mouse: Vector2, _attack_combo: int):
+	animation_tree.set("parameters/Sword_shield/conditions/is_attacking", is_attacking)
 	
+	#Thiết lập hướng đòn đánh
+	animation_tree.set("parameters/Sword_shield/Attack/Slash_1/blend_position", _mouse)
+	animation_tree.set("parameters/Sword_shield/Attack/Slash_2/blend_position", _mouse)
+	animation_tree.set("parameters/Sword_shield/Attack/Thrust/blend_position", _mouse)
+	animation_tree.set("parameters/Sword_shield/Attack/Shield_bash/blend_position", _mouse)
+	
+	#Thiết lập kiểu tấn công
+	animation_tree.set("parameters/Sword_shield/Attack/conditions/is_slash_1", _attack_combo == 0)
+	animation_tree.set("parameters/Sword_shield/Attack/conditions/is_slash_2", _attack_combo == 1)
+	animation_tree.set("parameters/Sword_shield/Attack/conditions/is_thrust", _attack_combo == 2)
+	animation_tree.set("parameters/Sword_shield/Attack/conditions/is_shield_bash", _attack_combo == 3)
+	
+	#Thiết lập hướng nhân vật sau khi thực hiện đòn đánh
+	animation_tree.set("parameters/Sword_shield/Idle/blend_position", _mouse)
+	animation_tree.set("parameters/Sword_shield/Move/blend_position", _mouse)
+	animation_tree.set("parameters/Sword_shield/Sheath/blend_position", _mouse)
+
+#Kết thúc thời gian để thực hiện combo
 func _on_attack_timer_timeout():
 	attack_combo = 0 
 
+#Bắt đầu hoạt ảnh tấn công
+func _attack_started():
+	attack_asset()
+
+#Kết thúc hoạt ảnh tấn công
 func _attack_finished():
 	is_attacking = false
-	animation_tree.set("parameters/conditions/is_attacking", is_attacking)
+	animation_tree.set("parameters/Sword_shield/conditions/is_attacking", is_attacking)
+	move_idle_asset()
+
+#Bắt đầu hoạt ảnh rút vũ khí
+func _draw_started():
+	change_state_asset()
+
+#Kết thúc hoạt ảnh rút vũ khí
+func _draw_finished():
+	is_drawing = false
+	animation_tree.set("parameters/conditions/is_drawing", is_drawing)
+	move_idle_asset()
+	is_attack_state = true
+
+#Bắt đầu hoạt ảnh cất vũ khí
+func _sheath_started():
+	change_state_asset()
+
+#Kết thúc hoạt ảnh cất vũ khí
+func _sheath_finished():
+	is_sheathing = false
+	animation_tree.set("parameters/Sword_shield/conditions/is_sheathing", is_sheathing)
+	base_asset()
+
+func base_asset():
 	base.texture = load("res://assets/characters/base/standMovePush/humn_v00.png")
 	one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/standMovePush/axe_v00.png")
 	shield.texture = load("res://assets/weapon/swordAndShield/standMovePush/shield_v00.png")
+
+func change_state_asset():
+	base.texture = load("res://assets/characters/swordAndShieldBase/changeState/humn_v00.png")
+	one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/changeState/axe_v00.png")
+	shield.texture = load("res://assets/weapon/swordAndShield/changeState/shield_v00.png")
+
+func attack_asset():
+	base.texture = load("res://assets/characters/swordAndShieldBase/attack/humn_v00.png")
+	one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/attack/axe_v00.png")
+	shield.texture = load("res://assets/weapon/swordAndShield/attack/shield_v00.png")
+
+func move_idle_asset():
+	base.texture = load("res://assets/characters/swordAndShieldBase/moveIdle/humn_v00.png")
+	one_hand_weapon.texture = load("res://assets/weapon/swordAndShield/moveIdle/axe_v00.png")
+	shield.texture = load("res://assets/weapon/swordAndShield/moveIdle/shield_v00.png")
